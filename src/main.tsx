ec8eff40cad2be1f5c783bb4eb13fdec1cf5b9e3
Adam Brawner <abrawner@ars.com>
2026-02-05 14:18:02 -0600

    remove css

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
-createRoot(document.getElementById("root")!).render(
-	<StrictMode>
+const root = document.getElementById("root");
+if (!root) throw new Error("Failed to find the root element");
+
+ReactDOM.createRoot(root).render(
+	<React.StrictMode>
 		<App />
-	</StrictMode>,
+	</React.StrictMode>,
 );
