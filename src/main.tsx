0221c4b34c54f03277a94fe040f81cbab9d4d594
Adam Brawner <abrawner@ars.com>
2026-02-05 16:24:04 -0600

    SignIn user context and error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { UserContextProvider } from "./context/UserContext";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Failed to find the root element");
 
 ReactDOM.createRoot(root).render(
 	<React.StrictMode>
-		<App />
+		<ErrorBoundary showError>
+			<UserContextProvider>
+				<App />
+			</UserContextProvider>
+		</ErrorBoundary>
 	</React.StrictMode>,
 );
